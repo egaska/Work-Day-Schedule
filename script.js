7e3d012af61d0700cae3a4b6c1e2ca5ce0176f90
Emily Gaska <emilygaska@gmail.com>
2020-04-30 08:46:40 -0500

    Created the timeBlocks in HTML for ease of formatting.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var currentDayEl = document.getElementById("currentDay");
 var blockTime = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 //Calling Functions
-drawBlocks();
+// drawBlocks();
 
 //Gets the current date using moment()
 function getDate() {
@@ -21,27 +21,22 @@ function getCurrentTime() {
     return moment().hour();
 }
 
-//Creates the hour blocks, assigns an hour id. Adds a counter
-function drawBlocks() {
-    for (let i = 0; i < blockTime.length; i++) {
-        var hourBlock = document.createElement("div");
-        hourBlock.setAttribute("class", "hourBlock");
-        hourBlock.setAttribute("id", blockTime[i]);
-        hourBlock.innerText = blockTime[i] + ":";
-        $(".container").append(hourBlock);
-    }
-    drawButtons();
-}
+// //Creates the hour blocks, assigns an hour id. Adds a counter
+// function drawBlocks() {
+//     for (let i = 0; i < blockTime.length; i++) {
+//         var hourBlock = document.createElement("div");
+//         var submitButton = document.createElement("button");
+//         submitButton.setAttribute("class", "submitButton");
+//         submitButton.setAttribute("id", blockTime[i] + " button")
+//         hourBlock.setAttribute("class", "hourBlock");
+//         hourBlock.setAttribute("id", blockTime[i]);
+//         hourBlock.innerText = blockTime[i] + ":";
+//         submitButton.innerText = "Update"
+//         $(".container").append(hourBlock);
+//         $(hourBlock).append(submitButton);
+//     }
+// }
 
-function drawButtons() {
-    var submitButton = document.createElement("button");
-    submitButton.setAttribute("class", "submitButton");
-    submitButton.setAttribute("id", blockTime[i] + " button")
-    $(".hourBlock").append(submitButton);
-    // I want to add a button to each div, but to put it in I need to refrence the class
-    //which is stored in an array. Can I do this?
-
-}
 
 
 
